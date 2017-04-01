52fb955a22b21cecbec98d4047257e9b2ee054ef
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-03-31 19:48:13 -0500

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function() {
 
+    //this array helps me have a way to name images the same way as a unique id for each character
     var characters = ["batman", "ironman", "superman", "wolverine"];
 
+    //I created this array in case the full names of the characters were longer or had spaces (e.g. Darth Sidius)
     var characterFullNames = ["Batman","Ironman","Superman","Wolverine"];
 
     var enemiesFought = [];
@@ -16,16 +18,22 @@ $(document).ready(function() {
     var enemyChosen = false;
 
     var attackHealth;
+
     var defenseHealth; 
+
     var attackDamage; 
+
     var defenseDamage;
 
+    // this counter will help deterine how powerful the next attack will be
     var attackCounter = 1;
 
     var enemiesDefeated = 0;
 
     var endOfGame = false;
 
+    //this function creates most of the html. I did it this way in order to prevent copy pasting the same block of code in my html file
+    //It is also helpful as a way to restart the game by just calling a single function 
     var startGame = function(){
         for(var x = 0; x < characters.length; x++){
 
@@ -69,6 +77,9 @@ $(document).ready(function() {
 
     startGame();
 
+
+    //choosing our character
+
     $(document).on("click", ".characterAvailable", function(event){
 
         $(".hideFirst").show();
@@ -88,8 +99,12 @@ $(document).ready(function() {
     });
 
 
+    //choosing our enemy
+
     $(document).on("click", ".myEnemies",function(event){
 
+
+        //If we have not chosen an enemy yet
 
         if(enemyChosen === false && characterChosen === true){
             $(".defender").append($(this));
@@ -98,6 +113,9 @@ $(document).ready(function() {
             enemiesFought.push($(this).attr("fullName"));
             enemyChosen = true;
         }
+
+        //This only helps the page determine how it should look depending on how many enemies are left
+       
         if(enemiesFought.length === 1){
             $(this).removeClass("col-xs-4");
             $(this).addClass("col-xs-12");
@@ -113,7 +131,11 @@ $(document).ready(function() {
 
     });
 
+    //when we click the attack button
+
     $(document).on("click", ".attack",function(event){
+
+        //remind the user to pick a character if they are pressing the button too soon
 
         if(characterChosen === false && endOfGame === false){
             var chooseFirst = $(".chooseReminder").html(
@@ -121,6 +143,8 @@ $(document).ready(function() {
             );
             chooseFirst.show(2000).hide(2000);
         }
+
+        //remind the user to pick an enemy if they are pressing the button too soon
 
         if(characterChosen === true && enemyChosen === false){
             var chooseFirst = $(".chooseReminder").html(
@@ -131,20 +155,27 @@ $(document).ready(function() {
 
         if(characterChosen === true && enemyChosen === true){
 
+            //update some variables for enemy and user character
             attackHealth = $(".myCharacter").attr("value");
             defenseHealth = $(".currentEnemy").attr("value");
             attackDamage = $(".myCharacter").attr("power")*attackCounter;
             defenseDamage = $(".currentEnemy").attr("power");
+
+            //update the enemy's health first because the user attacks first
             defenseHealth = parseInt(defenseHealth) - parseInt(attackDamage);
 
+            //print what just happened
             $(".summaryOfEvents").html(
                 "<p>You attacked "+ $(".currentEnemy").attr("fullName") + " for " + attackDamage + " damage!</p>" +
                 "<p>"+ $(".currentEnemy").attr("fullName") + " attacked you for " + defenseDamage + " damage!</p>"
             );
 
+            //if enemy is defeated
             if(defenseHealth <= 0){
                 enemiesDefeated++;
-                console.log(enemiesDefeated);
+                
+                //if the number of enemies defeated is equal to the number of enemies at the beginning, you won! 
+                //need to reset some values to keep the click events from triggering something
                 if(enemiesDefeated === characters.length-1){
                     $(".summaryOfEvents").html(
                     "<p>You Won!!! GAME OVER!!! reset the game to play again.</P>");
@@ -155,6 +186,8 @@ $(document).ready(function() {
                     $(".defender").empty();
                     
                 }
+
+                //if there are more enemies left, we empty the "defender" div and make the user pick another enemy
                 else{
                     $(".summaryOfEvents").html(
                     "<p>You have defeated: " + $(".currentEnemy").attr("fullName") + "</p>");
@@ -163,7 +196,11 @@ $(document).ready(function() {
                     defenseDamage = 0; 
                 }
             }
+
+            //if the enemy was not defeated after the user's attack, the user's character's health gets decreased
             attackHealth = parseInt(attackHealth) - parseInt(defenseDamage);
+
+            //after both attacks have happened, we update the attribute values and print them on the screen
             $(".myCharacter").attr("value",attackHealth);
             $(".currentEnemy").attr("value",defenseHealth);
             attackCounter++;
@@ -180,6 +217,8 @@ $(document).ready(function() {
         }
 
     });   
+
+    //if we click the reset button, we reset a few variables and use startGame function that was defined at the beginning
 
     $(".reset").on("click", function(){
 
